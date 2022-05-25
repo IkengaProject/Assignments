31c7658657a565259fe9b8ed5ce72a9e2d9dbb15
MannyProblems <emmanuel.munoz100@gmail.com>
2022-05-24 23:13:20 -0700

    I don't remember what I did but lets go!!!

--- Genin_Exam/Manny/js/fetch.js
+++ Genin_Exam/Manny/js/fetch.js
@@ -1,5 +1,9 @@
+let myB = document.getElementById('pokeButton')
+let searchPoke=()=> {
+let querey=""
 let exampleObj = {}
-fetch('https://pokeapi.co/api/v2/')
+querey= document.getElementById('pokeText').value
+fetch('https://pokeapi.co/api/v2/pokemon/'+querey)
     .then((response) => {
         console.log(response)
         return response.text()
@@ -7,7 +11,16 @@ fetch('https://pokeapi.co/api/v2/')
     .then((data) => {
         console.log(data)
         let jsonData = JSON.parse(data)
+        document.getElementById("weight").innerHTML=jsonData.weight
         var image = document.getElementById('example')
-
+        image.src = jsonData.sprites.other["official-artwork"].front_default
+        console.log(jsonData.sprites);
         exampleObj = jsonData
     });
+  //  .then(()=>{
+    //  fetch()
+
+
+  //  })
+}
+myB.addEventListener("click",searchPoke)
